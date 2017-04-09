9fcbd8d48504a4f0867ce10ea8dd23c7ba6276c9
Bit Second Tech <bitsecondal@gmail.com>
2017-04-09 14:01:17 -0500

    All on and off (#3)
    * Created all on and all off scripts as well as defined the all on and off functions in raspitraffic.py
    
    * Update LCD message for all on. Updated function name in the run_all_on script
    
    * Created .gitignore file. Updated script header comments. Created terminate function to be used when exception is called.
    
    * Updated .gitignore file
    
    * Disabled DEBUG mode
    
    * Removed whitespace that awas requested from pull request #3

--- raspitraffic.py
+++ raspitraffic.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 ################################################################################
-# Project: 	Traffic Control
-# Script Usage: run_flasher.py
+# Project: 	Raspi Traffic Control
+# Script Usage: raspitraffic.py
 # Created: 	2017-04-02
 # Author: 	Kenny Robinson, Bit Second Tech (www.bitsecondtech.com)
-# Description:	Flash lights as if a malfunction or power outage has occurred.
+# Description:	Core script which all the functions for controlling the lights
+# 		are contained. 
 ################################################################################
 
 from time import sleep
 import RPi.GPIO as GPIO
 import lcddriver
 import random
+
+# DEBUG MODE, ENABLED=1, DISABLED=0
+DEBUG=0
 
 # LIST ALL OF THE PINS USED
 pinOutList = [37, 35, 33, 31, 29, 23, 21, 19]
@@ -27,9 +31,6 @@ NORTH_LY = 29
 EAST_CR = 23
 EAST_CY = 21
 EAST_CG = 19
-
-# DEBUG MODE, ENABLED=1, DISABLED=0
-DEBUG=1
 
 display=lcddriver.lcd()
 ALL_RED_TIME=5
@@ -79,6 +80,12 @@ def lcd_message(line1, line2):
 	display.lcd_display_string(line2, 2)
 	log_message(line1 + " | " + line2)
 	return 0
+
+def terminate():
+# WHEN COMMAND TO EXIT IS GIVEN, THEN RESET EVERYTHING BACK TO DEFAULT
+	log_message("Exiting")
+        GPIO.cleanup()
+        display.lcd_clear()
 
 def controlflasher( phase ):
 # runs flasher SEQUENCE
@@ -205,6 +212,20 @@ def randomspeed():
 	speed=random.randint(25,50)
 	return speed
 
+def allon():
+# TURNS ON ALL OF THE LIGHTS
+	lcd_message("ALL LIGHTS ON", "")
+	for i in pinOutList:
+		light_on(i)
+	sleep(3)
+
+def alloff():
+# TURNS OFF ALL OF THE LIGHTS
+	lcd_message("ALL LIGHTS OFF", "")
+	for i in pinOutList:
+		light_off(i)
+	sleep(3)
+
 def lamptest():
 	lcd_message("LAMP TEST", "")
 
@@ -225,5 +246,4 @@ def lamptest():
 	sleep(3)
 	sleep(3)	
 	display.lcd_clear()
-
 
---
+++ run_all_on.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+################################################################################
+# Project: 	Raspi Traffic Control
+# Script Usage: run_all_on.py
+# Created: 	2017-04-09
+# Author: 	Kenny Robinson, Bit Second Tech (www.bitsecondtech.com)
+# Description:	Turns on all the lights and exits.
+################################################################################
+
+import raspitraffic as rtc
+
+try:
+	rtc.setup()
+	rtc.allon()
+except KeyboardInterrupt:
+	rtc.terminate()
