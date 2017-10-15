6f76cc4201acb528e2d4131344de9aa9b54acd07
Bit Second <bitsecondal@gmail.com>
2017-10-15 13:24:14 -0500

    Removed excess comments. Combined functions where possible.

--- raspitraffic.py
+++ raspitraffic.py
@@ -24,26 +24,20 @@ DEBUG=0
 # pinOutList = [37, 35, 33, 31, 29, 23, 21, 19]
 pinOutList = [23, 21, 19]
 
-# DEFINE THE GPIO NUMBERS AND VARIABLES FOR NORTHBOUND TRAFFIC
-# NORTH_CR = 37
-# NORTH_CY = 35
-# NORTH_CG = 33
-# NORTH_LG = 31
-# NORTH_LY = 29
-
 # DEFINE THE GPIO NUMBERS AND VARIABLES FOR THE EASTBOUND TRAFFIC
 EAST_CR = 23
 EAST_CY = 21
 EAST_CG = 19
 
 # DEFINE CONSTANTS
-LMPON=GPIO.LOW
-LMPOFF=GPIO.HIGH
+LAMPON=GPIO.LOW
+LAMPOFF=GPIO.HIGH
 ALL_RED_TIME=2
 FLASHER_DELAY=.7
 
 display=lcddriver.lcd()
 selection=0
+
 
 def setup():
     # SET UP GPIO PINS
@@ -73,11 +67,13 @@ def setup():
 
     return 0
 
+
 def light_on(pin):
 # TURN ON THE LIGHT, HAS TO PROVIDE PIN NUMBER
     GPIO.output(pin, GPIO.LOW)
     debug_message("Pin " + str(pin) + " turned on")
     return 0
+    
 
 def light_off(pin):
 # TURN OFF THE LIGHT, HAS TO PROVIDE PIN NUMBER
@@ -85,16 +81,19 @@ def light_off(pin):
     debug_message("Pin " + str(pin) + " turned off")
     return 0
 
+
 def debug_message(message):
 # LOG ADDITIONAL MESSAGES TO THE SCREEN/LOG FILE WHEN TESTING
     if DEBUG == 1:
         log_message("DEBUG: " + message)
     return 0
+    
 
 def log_message(message):
 # print message on computer screen
     print message
     return 0
+
 
 def lcd_message(line1, line2):
 # Displays the message on the LCD screen and computer screen
@@ -104,48 +103,90 @@ def lcd_message(line1, line2):
     # log_message(line1 + " | " + line2)
     return 0
 
+
 def terminate():
 # WHEN COMMAND TO EXIT IS GIVEN, THEN RESET EVERYTHING BACK TO DEFAULT
     log_message("Exiting")
     GPIO.cleanup()
     display.lcd_clear()
+   
 
-def run_us_signal():
+def run_red_light_green_light():
+# SEQUENCE FOR RED LIGHT GREEN LIGHT GAME.
+
+    # phase 0 - do nothing
+    # phase 1 - red light
+    # phase 2 - green light
+    
+    phasenum=1
+
+    while True:
+        red_time=randint(2, 10)
+	    green_time=randint(1, 3)
+
+	    phasenum=rtc.controlredlightgreenlight(phasenum)
+	    rtc.debug_message("Red Time: " + str(red_time))
+	    rtc.lcd_message("Red Light!", "Dont move!")
+	    sleep(red_time)
+		
+	    phasenum=rtc.controlredlightgreenlight(phasenum)
+	    rtc.debug_message("Green Time: " + str(green_time))
+	    rtc.lcd_message("Green Light!", "Run!")
+	    sleep(green_time)
+
+        
+def run_signal(country):
+# runs the light using normal signal
     while True:
         phasering1=1
         phaseflasher=0
 
-    north_grn_time=calc_green_time()
-    north_yel_time=calc_yellow_time(randomspeed(), random.randint(0, 5))
-    east_grn_time=calc_green_time()
-    east_yel_time=calc_yellow_time(randomspeed(), random.randint(0, 5))
+        east_grn_time=random.randint(7, 60)
+        east_yel_time=random.randint(2, 5)
+        east_red_time=random.randint(7, 60)
 
-    # green 
-    phasering1=controlring1(phasering1)
-    for ttime in range(north_grn_time, 0, -1):
-        lcd_message("Green", "Time Remain: " + str(ttime) + "s")
-        sleep(1)
+        # red
+        eblight(LAMPON, LAMPOFF, LAMPOFF)
+        for ttime in range(east_red_time, 0, -1):
+	        lcd_message("Red", "Time Remain: " + str(ttime) + "s")
+	        sleep(1)
+	        
+	    if country == "UK":
+            eblight(LAMPON, LAMPON, LAMPOFF)
+            for ttime in range(east_yel_time, 0, -1):
+	            lcd_message("Red-Yellow", "Time Remain: " + str(ttime) + "s")
+	            sleep(1)
+	        
+        # green 
+        eblight(LAMPOFF, LAMPOFF, LAMPON)
+        for ttime in range(east_grn_time, 0, -1):
+	        lcd_message("Green", "Time Remain: " + str(ttime) + "s")
+	        sleep(1)
 
-    # yellow 
-    phasering1=controlring1(phasering1)
-    for ttime in range(int(round(north_yel_time, 0)), 0, -1):
-        lcd_message("Yellow", "Time Remain: " + str(ttime) + "s")
-        sleep(1)
+        # yellow
+        eblight(LAMPOFF, LAMPON, LAMPOFF)
+        # for ttime in range(int(round(north_yel_time, 0)), 0, -1):
+        for ttime in range(east_yel_time, 0, -1):
+	        lcd_message("Yellow", "Time Remain: " + str(ttime) + "s")
+	        sleep(1)
+	        
+        # red
+        # phasering1=controlring1(phasering1)
+        eblight(LAMPON, LAMPOFF, LAMPOFF)
+        for ttime in range(east_red_time, 0, -1):
+	        lcd_message("Red", "Time Remain: " + str(ttime) + "s")
+	        sleep(1)
 
-    # all red phase
-    phasering1=controlring1(phasering1)
-    for ttime in range(ALL_RED_TIME, 0, -1):
-        lcd_message("Red", "Time Remain: " + str(ttime) + "s")
-        sleep(1)
+        # change flasher color
+        if phaseflasher == 1 or phaseflasher == 2:
+            phaseflasher = 3
+        else:
+            phaseflasher = 1
 
-    # change flasher color
-    if phaseflasher == 1 or phaseflasher == 2:
-        phaseflasher = 3
-    else:
-        phaseflasher = 1
-
-    # flasher
-    #for 
+        # flasher
+        for i in 20:
+            phaseflasher = controlflasher(phaseflasher)
+            sleep(1)
 
 
 def controlflasher(phase):
@@ -156,54 +197,54 @@ def controlflasher(phase):
 
     # flash red
     elif phase == 1:
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPOFF)
+        # nblight(LAMPOFF, LAMPON, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPOFF, LAMPOFF, LAMPOFF)
         # lcd_message("Flasher Mode", "EB OFF, NB YEL")
         phase=2
     elif phase == 2:
-        # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
+        # nblight(LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPON, LAMPOFF, LAMPOFF)
         # lcd_message ("Flasher Mode", "EB RED, NB OFF")
         phase=1
 
     # # flash red lights
     # elif phase == 3:
-    # # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-    # eblight(LMPOFF, LMPOFF, LMPOFF)
+    # # nblight(LAMPON, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+    # eblight(LAMPOFF, LAMPOFF, LAMPOFF)
     # phase=4
     # elif phase == 4:
-    # # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-    # eblight(LMPON, LMPOFF, LMPOFF)
+    # # nblight(LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+    # eblight(LAMPON, LAMPOFF, LAMPOFF)
     # phase=3
 
     # flash yellow lights
     elif phase == 5:
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPOFF)
+        # nblight(LAMPOFF, LAMPON, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPOFF, LAMPOFF, LAMPOFF)
         phase = 6
     elif phase == 6:
-        # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPON, LMPOFF)
+        # nblight(LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPOFF, LAMPON, LAMPOFF)
         phase = 5
 
     # flash green lights
     elif phase == 7:
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPON)
-        eblight(LMPOFF, LMPOFF, LMPOFF)
+        # nblight(LAMPOFF, LAMPOFF, LAMPON, LAMPOFF, LAMPON)
+        eblight(LAMPOFF, LAMPOFF, LAMPOFF)
         phase = 8
     elif phase == 8:
-        # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPON)
+        # nblight(LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPOFF, LAMPOFF, LAMPON)
         phase = 7
 
     # # flash with nb red, eb yellow
     # elif phase == 9:
-    # # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-    # eblight(LMPOFF, LMPOFF, LMPOFF)
+    # # nblight(LAMPON, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+    # eblight(LAMPOFF, LAMPOFF, LAMPOFF)
     # phase = 10
     # elif phase == 10:
-    # # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-    # eblight(LMPOFF, LMPON, LMPOFF)
+    # # nblight(LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+    # eblight(LAMPOFF, LAMPON, LAMPOFF)
     # phase = 9
 
     else:
@@ -219,190 +260,28 @@ def calc_yellow_time( speed, grade ):
     # log_message("Yellow Time: " + str(yel_time))
     return yel_time
 
+
 def calc_green_time():
 # SET A RANDOM VALUE FOR THE GREEN TIME
     grn_time=random.randint(10, 45)
     # log_message("Green Time: " + str(grn_time))
     return grn_time
 
-# def # nblight(cirred, ciryel, cirgrn, lefyel, lefgrn):
-# # CONTROLS THE LAMPS ON THE NORTHBOUND LIGHT. DESIGNED TO INCLUDE LEFT TURN
-# GPIO.output(NORTH_CR, cirred)
-# GPIO.output(NORTH_CY, ciryel)
-# GPIO.output(NORTH_CG, cirgrn)
-# GPIO.output(NORTH_LY, lefyel)
-# GPIO.output(NORTH_LG, lefgrn)
 
 def eblight(cirred, ciryel, cirgrn):
 # CONTROLS THE LAMPS ON THE EASTBOUND LIGHT. DOESNT HAVE LEFT TURN
     GPIO.output(EAST_CR, cirred)
     GPIO.output(EAST_CY, ciryel)
-    GPIO.output(EAST_CG, cirgrn)
+    GPIO.output(EAST_CG, cirgrn)	
 
-def controlring1uk(phase):
-# RUN NORMAL SEQUENCE WITH UK PHASING
-
-    # phase 0 - do nothing
-    # phase 1 - nb cr cy, eb cr
-    # phase 2 - nb cg, eb cr
-    # phase 3 - nb cy, eb cr
-    # phase 4 - nb cr, eb cr cy
-    # phase 5 - nb cr, eb cg
-    # phase 6 - nb cr, eb cy
-
-    debug_message("Incoming phase: " + str(phase))
-
-    if phase == 0:
-        log_message("Do nothing")
-    elif phase == 1:
-        # nblight(LMPON, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        phase = 2
-    elif phase == 2:
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        phase=3
-    elif phase == 3:
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        phase = 4
-    elif phase == 4:
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPON, LMPOFF)
-        phase = 5
-    elif phase == 5:
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPON)
-        phase = 6
-    elif phase == 6:
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPON, LMPOFF)
-        phase = 1 
-    else:
-        phase=1
-
-    debug_message("Outgoing phase " + str(phase))
-
-    return phase
-
-def controlredlightgreenlight(phase):
-# SEQUENCE FOR RED LIGHT GREEN LIGHT GAME. PLAYED WITH ONLY ONE TRAFFIC LIGHT.
-
-    # phase 0 - do nothing
-    # phase 1 - red light
-    # phase 2 - green light
-    if phase == 0:
-        log_message("Doing nothing")
-    elif phase == 1:
-        # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        lcd_message("Red Light!", "")
-        phase = 2
-    elif phase == 2:
-        # nblight(LMPOFF, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPON)
-        lcd_message("Green Light!", "")
-        phase = 1
-    else:
-        phase = 1
-        log_message("Not a valid phase")
-
-    return phase
-
-
-def controlring1(phase):
-# RUN NORMAL SEQUENCE FOR NORTHBOUND AND EASTBOUND LIGHT
-
-    # phase 0 - do nothing
-    # phase 1 - nb cg, eb cr
-    # phase 2 - nb cy, eb cr
-    # phase 3 - nb cr, eb cr
-    # phase 4 - nb cr, eb cg
-    # phase 5 - nb cr, eb cy
-    # phase 6 - nb cr, eb cr
-    debug_message("incoming phase: " + str(phase))
-    nextphase = 0
-
-    if phase == 0:
-        log_message("Doing nothing")
-    elif phase == 1:
-        log_message("NB GRN -- EB RED")
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        nextphase = 2
-    elif phase == 2:
-        log_message("NB YEL -- EB RED")
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        nextphase = 3
-    elif phase == 3:
-        log_message("NB RED -- EB RED")
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        nextphase = 4
-    elif phase == 4:
-        log_message("NB RED -- EB GRN")
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPON)
-        nextphase = 5
-    elif phase == 5:
-        log_message("NB RED -- EB YEL")
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPON, LMPOFF)
-        nextphase = 6
-    elif phase == 6:
-        log_message("NB RED -- EB RED")
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        nextphase = 1
-    else:
-        nextphase = 1
-
-    debug_message("Outgoing phase: " + str(nextphase))
-    return nextphase
-
-def controlring2(phase):
-# NORMAL SEQUENCE FOR CONTROLLING NORTHBOUND AND SOUTHBOUND TRAFFIC
-    # phase 0 - do nothing
-    # phase 1 - nb cg, sb cg
-    # phase 2 - nb cy, sb cy
-    # phase 3 - nb cr, sb cr
-    # phase 4 - nb cg lg, sb cr
-    # phase 5 - nb cg ly, sb cr 
-
-    if phase == 0:
-        log_message("Doing nothing")
-    elif phase == 1:
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPOFF, LMPON)
-        phase = 2
-    elif phase == 2:
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPOFF, LMPON, LMPOFF)
-        phase = 3
-    elif phase == 3:
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        phase = 4
-    elif phase == 4:
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPON)
-        eblight(LMPOFF, LMPOFF, LMPOFF)
-        phase = 5
-    elif phase == 5:
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPON, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
-        phase = 1
-    else:
-        log_message("Invalid phase")
-        phase = 1
-    return phase 
 
 def randomspeed():
 # picks a random speed from the range defined below
     speed=random.randint(25,65)
     return speed
+    
 
-def controleightball():
+def run_eightball():
 # run the magic 8 ball game with the traffic light 
 
     # display prompts
@@ -417,13 +296,13 @@ def controleightball():
     rand=random.randint(1,3)
 
     if rand == 1:
-        eblight(LMPON, LMPOFF, LMPOFF)
+        eblight(LAMPON, LAMPOFF, LAMPOFF)
         lcd_message("No","")
     elif rand == 2:
-        eblight(LMPOFF, LMPON, LMPOFF)
+        eblight(LAMPOFF, LAMPON, LAMPOFF)
         lcd_message("Maybe", "")
     elif rand == 3:
-        eblight(LMPOFF, LMPOFF, LMPON)
+        eblight(LAMPOFF, LAMPOFF, LAMPON)
         lcd_message("Yes", "")
     else:
         log_message("Invalid Phase")
@@ -432,26 +311,27 @@ def controleightball():
 def allon(phase):
 # TURNS ON THE LIGHTS BASED ON THE ARGUMENT PROVIDED 
     if phase == "all":
-        # nblight(LMPON, LMPON, LMPON, LMPON, LMPON)
-        eblight(LMPON, LMPON, LMPON)
+        # nblight(LAMPON, LAMPON, LAMPON, LAMPON, LAMPON)
+        eblight(LAMPON, LAMPON, LAMPON)
         lcd_message("ALL LIGHTS ON", "")
     elif phase == "red":
-        # nblight(LMPON, LMPOFF, LMPOFF, LMPOFF, LMPOFF)
-        eblight(LMPON, LMPOFF, LMPOFF)
+        # nblight(LAMPON, LAMPOFF, LAMPOFF, LAMPOFF, LAMPOFF)
+        eblight(LAMPON, LAMPOFF, LAMPOFF)
         lcd_message("ALL REDS ON", "")
     elif phase == "yellow":
-        # nblight(LMPOFF, LMPON, LMPOFF, LMPON, LMPOFF)
-        eblight(LMPOFF, LMPON, LMPOFF)
+        # nblight(LAMPOFF, LAMPON, LAMPOFF, LAMPON, LAMPOFF)
+        eblight(LAMPOFF, LAMPON, LAMPOFF)
         lcd_message("ALL YELLOWS ON", "")
     elif phase == "green":
-        # nblight(LMPOFF, LMPOFF, LMPON, LMPOFF, LMPON)
-        eblight(LMPOFF, LMPOFF, LMPON)
+        # nblight(LAMPOFF, LAMPOFF, LAMPON, LAMPOFF, LAMPON)
+        eblight(LAMPOFF, LAMPOFF, LAMPON)
         lcd_message("ALL GREENS ON", "")
     else:
         log_message("Doing nothing")
 
     sleep(3)
     display.lcd_clear()
+
 
 def alloff():
 # TURNS OFF ALL OF THE LIGHTS
@@ -461,6 +341,7 @@ def alloff():
     
     sleep(3)
     display.lcd_clear()
+
 
 def lamptest():
     lcd_message("LAMP TEST", "")
@@ -481,6 +362,7 @@ def lamptest():
     lcd_message("LAMP TEST", "ALL OFF")
     sleep(3)
     display.lcd_clear()
+
 
 def mainmenu():
 # MAIN MENU FOR THE PROGRAM
