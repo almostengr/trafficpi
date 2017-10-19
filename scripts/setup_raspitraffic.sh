8b740d40ca8b69941debc91ae2e4128c06304180
Bit Second <bitsecondal@gmail.com>
2017-10-18 20:59:29 -0500

    Merged initial setup scripts

--- scripts/setup_raspitraffic.sh
+++ scripts/setup_raspitraffic.sh
@@ -19,31 +19,37 @@ function log_message() {
 if [ $(id -u) -eq 0 ]; then
 	log_message "Running script"
 
-	log_message "Updating repositories"
+	log_message "Updating software packages"
 
-	apt-get update
+	/usr/bin/apt-get update
 	
-	log_message "Done updating repositories"
+	log_message "Done updating software packages"
 	
-	log_message "Upgrading repositories"
+	log_message "Upgrading software packages"
 
-	apt-get upgrade -y 
-	
-	log_message "Done upgrading respositories"
+	/usr/bin/apt-get upgrade -y 
+
+	log_message "Done upgrading software packages"
 
 	log_message "Installing needed packages" 
 
-	apt-get python
+	/usr/bin/apt-get python git
 	
 	log_message "Done installing needed packages"
 	
 	log_message "Displaying python version"
 
 	/usr/bin/python --version
+	
+	log_message "Done displaying python version"
 
+	log_message "Running LCD install script"
+
+	# run LCD install script
+	. $(pwd)/lcd/install.sh
+
+	log_message "Done running LCD install script"
 else
 	log_message "Need to be sudo or root to run script"
-if
-
-log_message "Done running script"
+fi
 
