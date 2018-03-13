bedf0b4fa2d6e6c4b21e55c23d7e13447c2a7064
Bit Second <bitsecondal@gmail.com>
2018-03-13 02:44:47 +0000

    Added form for controling light

--- web/index.php
+++ web/index.php
@@ -3,16 +3,55 @@
 <title>Traffic Control</title>
 </head>
 <body>
-<ul>
+
 <?php
 
-$scripts = scandir('/home/pi/raspitraffic-stem/*py');
+// write the file if something is selected
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$action = "";
 
-foreach ($scripts as $script) {
-	echo '<li>$script</li>';
+	if (isset($_POST['us'])) {
+		$action = "us";
+	}
+	else if (isset($_POST['uk']) {
+		$action = "uk";
+	}
+	else if (isset($_POST['allon']) {
+		$action = "allon";
+	}
+	else if (isset($_POST['alloff']) {
+		$action = "alloff";
+	}
+	else if (isset($_POST['flashred']) {
+		$action = "flashred";
+	}
+	else if (isset($_POST['flashyel']) {
+		$action = "flashyel";
+	}
+	else if (isset($_POST['flashgrn']) {
+		$action = "flashgrn";
+	}
+	else if (isset($_POST['shutdown']) {
+		$action = "shutdown";
+	}
+
+	$file = fopen("/tmp/traffic.txt", "w");
+	fwrite($file, $action);
+	fclose($file);
 }
-
 ?>
+
+<form method="post">
+<p><input type="submit" name="us" value="US Traffic" /></p>
+<p><input type="submit" name="uk" value="UK Traffic" /></p>
+<p><input type="submit" name="allon" value="All On" /></p>
+<p><input type="submit" name="alloff" value="All Off" /></p>
+<p><input type="submit" name="flashred" value="Flash Red" /></p>
+<p><input type="submit" name="flashyel" value="Flash Yellow" /></p>
+<p><input type="submit" name="flashgrn" value="Flash Green" /></p>
+<p>&nbsp;</p>
+<p><input type="submit" name="shutdown" value="Shut Down" /></p>
+</form>
 
 </body>
 </html>
