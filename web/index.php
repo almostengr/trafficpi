883cdc0e1b456c1f4cea69560a2f61899d6c29d9
Bit Second <bitsecondal@gmail.com>
2018-03-18 17:54:41 +0000

    Fixed coding errors were page would not load

--- web/index.php
+++ web/index.php
@@ -4,42 +4,45 @@
 </head>
 <body>
 
-Page body
-
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $action = "";
 
-// write the file if something is selected
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$action = "";
+        if (isset($_POST['us'])) {
+                $action = "us";
+        }
+        else if (isset($_POST['uk'])) {
+                $action = "uk";
+        }
+        else if (isset($_POST['allon'])) {
+                $action = "allon";
+        }
+        else if (isset($_POST['alloff'])) {
+                $action = "alloff";
+        }
+        else if (isset($_POST['flashred'])) {
+                $action = "flashred";
+        }
+        else if (isset($_POST['flashyel'])) {
+                $action = "flashyel";
+        }
+        else if (isset($_POST['flashgrn'])) {
+                $action = "flashgrn";
+        }
+        else if (isset($_POST['shutdown'])) {
+                $action = "shutdown";
+        }
 
-	if (isset($_POST['us'])) {
-		$action = "us";
-	}
-	else if (isset($_POST['uk']) {
-		$action = "uk";
-	}
-	else if (isset($_POST['allon']) {
-		$action = "allon";
-	}
-	else if (isset($_POST['alloff']) {
-		$action = "alloff";
-	}
-	else if (isset($_POST['flashred']) {
-		$action = "flashred";
-	}
-	else if (isset($_POST['flashyel']) {
-		$action = "flashyel";
-	}
-	else if (isset($_POST['flashgrn']) {
-		$action = "flashgrn";
-	}
-	else if (isset($_POST['shutdown']) {
-		$action = "shutdown";
-	}
+	echo "<p style='background-color: #0c0; margin: 5px; padding: 5px;'>Running $action</p>";
 
-	$file = fopen("/tmp/traffic.txt", "w");
-	fwrite($file, $action);
-	fclose($file);
+	try {
+		// write the action to the file
+        	$file = fopen('/tmp/traffic.txt', 'w') or die("Can't open file");
+	        fwrite($file, $action) or die("Write failed");
+        	fclose($file);
+	} catch (Exception $e) {
+		echo "<p style='background-color: #c00; padding: 5px;'>$e->getMessage()</p>";
+	}
 }
 ?>
 
