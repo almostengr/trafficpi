b61ed30ce91cff49975329b07df012d5d748c9f5
Almost Engineer <bitsecondal@gmail.com>
2020-05-22 09:53:47 -0500

    #33 Added debug flag compiler flag

--- trafficpiconsole/Program.cs
+++ trafficpiconsole/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading;
 
-namespace trafficpiconsole
+namespace Almostengr.TrafficPi.ConsoleControl
 {
     class Program
     {
+        private static bool Debug = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+#if DEBUG
+            Debug = true;
+#endif
+
+            Console.WriteLine("Starting TrafficPi");
+            Console.WriteLine("Press Ctrl+C to exit");
+            Console.WriteLine();
         }
     }
 }
