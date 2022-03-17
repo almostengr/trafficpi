519d521321edbdcb27ee780c3b7bd186794d6d2f
Almost Engineer <bitsecondal@gmail.com>
2022-03-16 20:01:16 -0500

    #40 created constants class

---
+++ Almostengr.TrafficPi.Web/Constants/ProgramName.cs
@@ -0,0 +1,8 @@
+﻿namespace Almostengr.TrafficPi.Web.Constants
+{
+    public static class ProgramName
+    {
+        public static readonly string ShutDown = "shutdown";
+        public static readonly string Reboot = "reboot";
+    }
+}
