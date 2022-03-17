a46235cdde7623fdfe58b94f235b333703b6f4f0
Almost Engineer <bitsecondal@gmail.com>
2022-03-16 20:00:54 -0500

    #42 created mock classes for services

--- Almostengr.TrafficPi.LampControl/Services/MockGpioService.cs
+++ Almostengr.TrafficPi.LampControl/Services/MockGpioService.cs
@@ -1,1 +1,45 @@
+namespace Almostengr.TrafficPi.LampControl.Services
+{
+    public class MockGpioService : IGpioService
+    {
+        public void CloseOutputPin(int pinNumber)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public void CloseOutputPins(int[] pinNumbers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OpenOutputPin(int pinNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OpenOutputPins(int[] pinNumbers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TurnOffGpio(int pinNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TurnOffGpio(int[] pinNumbers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TurnOnGpio(int pinNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TurnOnGpio(int[] pinNumbers)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Almostengr.TrafficPi.LampControl/Services/MockSignalIndicationService.cs
+++ Almostengr.TrafficPi.LampControl/Services/MockSignalIndicationService.cs
@@ -1,1 +1,55 @@
+namespace Almostengr.TrafficPi.LampControl.Services
+{
+    public class MockSignalIndicationService : ISignalIndicationService
+    {
+        public void AllLights()
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public void GreenLight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InitializeLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NoLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RedGreenLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RedLight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RedYellowLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShutdownLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void YellowGreenLights()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void YellowLight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
